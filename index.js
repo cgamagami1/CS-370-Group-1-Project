04dbf1f11ce96b273bfef2e570b04ab4b59cda77
GamagamiDev <81793604+cgamagami1@users.noreply.github.com>
2023-05-02 01:16:06 -0500

    Extract import logic into seperate functions

--- index.js
+++ index.js
@@ -19,6 +19,77 @@ app.get("/import", function(req, res){
     res.render("import", { insertedRows: 0, updatedRows: 0, errors: [] });
 });
 
+const importUser = async ({ fName, lName, mInitial, biography, location, birthday }) => {
+    const [user] = await con.query("SELECT userID FROM user WHERE fName = ? AND mInitial = ? AND lName = ?", [fName, mInitial, lName]);
+    
+    try {
+        if (user.length > 0) {
+            await con.query(
+                "UPDATE user SET ? WHERE userID = ?", 
+                [{ fName, lName, mInitial, biography, location, birthday }, user[0].userID]
+            );
+            return { inserted: false, updated: true, error: null };
+        }
+        else {
+            await con.query(
+                "INSERT INTO user (mInitial, fName, lName, biography, location, birthday) VALUES (?)", 
+                [[mInitial, fName, lName, biography, location, birthday]]
+            );
+            return { inserted: true, updated: false, error: null };
+        }
+    }
+    catch (err) {
+        return { inserted: false, updated: false, error: err.message };
+    }
+}
+
+const importPost = async ({ likeCount, location, content, photoURL, userID, date, eventID, groupID, trendID }) => {
+    const [post] = await con.query("SELECT postID FROM post WHERE userID = ? AND content = ?", [userID, content]);
+
+    try {
+        if (post.length > 0) {
+            await con.query(
+                "UPDATE post SET ? WHERE postID = ?",
+                [{ likeCount, location, content, photoURL, userID, date, eventID, groupID, trendID }, post[0].postID]
+            );
+            return { inserted: false, updated: true, error: null };
+        }
+        else {
+            await con.query(
+                "INSERT INTO post (likeCount, location, content, photoURL, userID, date, eventID, groupID, trendID) VALUES (?)",
+                [[likeCount, location, content, photoURL, userID, date, eventID, groupID, trendID]]
+            );
+            return { inserted: true, updated: false, error: null };
+        }
+    }
+    catch (err) {
+        return { inserted: false, updated: false, error: err.message };
+    }
+}
+
+const importComment = async ({ userID, content, numLikes, date, postID }) => {
+    const [comment] = await con.query("SELECT commentID FROM comment WHERE userID = ? AND content = ? AND postID = ?", [userID, content, postID]);
+
+    try {
+        if (comment.length > 0) {
+            await con.query(
+                "UPDATE comment SET ? WHERE commentID = ?",
+                [{ userID, content, numLikes, date, postID }, comment[0].commentID]
+            );
+            return { inserted: false, updated: true, error: null };
+        }
+        else {
+            await con.query(
+                "INSERT INTO comment (userID, content, numLikes, date, postID) VALUES (?)",
+                [[userID, content, numLikes, date, postID]]
+            );
+            return { inserted: true, updated: false, error: null };
+        }
+    }
+    catch (err) {
+        return { inserted: false, updated: false, error: err.message };
+    }
+}
 
 app.post("/import", async (req, res) => {
     const isUserCsv = Boolean(req.files?.users_csv);
@@ -40,70 +111,21 @@ app.post("/import", async (req, res) => {
         }
     }
 
-    let insertedRows = 0;
-    let updatedRows = 0;
-
-    const results = await Promise.all(data.map(async d => {
-        let query = "";
-        let input = [];
-        let existingID = null;
-        
+    const results = await Promise.all(data.map(async d => {     
         if (isUserCsv) {
-            const [user] = await con.query("SELECT userID FROM user WHERE fName = ? AND mInitial = ? AND lName = ?", [d.fName, d.mInitial, d.lName]);
-            
-            if (user.length > 0) {
-                query = "UPDATE user SET ? WHERE userID = ?";
-                input = { mInitial: d.mInitial, fName: d.fName, lName: d.lName, biography: d.biography, location: d.location, birthday: d.birthday};
-                existingID = user[0].userID;
-            }
-            else {
-                query = "INSERT INTO user (mInitial, fName, lName, biography, location, birthday) VALUES (?)";
-                input = [d.mInitial, d.fName, d.lName, d.biography, d.location, d.birthday];
-            }
+            return await importUser(d);
         }
         else if (isPostCsv) {
-            const [post] = await con.query("SELECT postID FROM post WHERE userID = ? AND content = ?", [d.userID, d.content]);
-
-            if (post.length > 0) {
-                query = "UPDATE post SET ? WHERE postID = ?";
-                input = { likeCount: d.likeCount, location: d.location, content: d.content, photoURL: d.photoURL, userID: d.userID, date: d.date, eventID: d.eventID, groupID: d.groupID, trendID: d.trendID };
-                existingID = post[0].postID
-            }
-            else {
-                query = "INSERT INTO post (likeCount, location, content, photoURL, userID, date, eventID, groupID, trendID) VALUES (?)";
-                input = [d.likeCount, d.location, d.content, d.photoURL, d.userID, d.date, d.eventID, d.groupID, d.trendID];
-            }
+            return await importPost(d);
         }
         else if (isCommentCsv) {
-            const [comment] = await con.query("SELECT commentID FROM comment WHERE userID = ? AND content = ?", [d.userID, d.content]);
-
-            if (comment.length > 0) {
-                query = "UPDATE comment SET ? WHERE commentID = ?";
-                input = { userID: d.userID, content: d.content, numLikes: d.numLikes, date: d.date, postID: d.postID };
-                existingID = comment[0].commentID;
-            }
-            else {
-                query = "INSERT INTO comment (userID, content, numLikes, date, postID) VALUES (?)";
-                input = [d.userID, d.content, d.numLikes, d.date, d.postID];
-            }
-        }
-
-        try {
-            if (existingID === null) {
-                await con.query(query, [input]);
-                insertedRows++;
-            }
-            else {
-                await con.query(query, [input, existingID]);
-                updatedRows++;
-            }
-        }
-        catch (err) {
-            return err.message;
+            return await importComment(d);
         }
     }))
 
-    const errors = results.filter(result => result !== undefined);
+    const insertedRows = results.reduce((acc, result) => result.inserted ? acc + 1 : acc, 0);
+    const updatedRows = results.reduce((acc, result) => result.updated ? acc + 1 : acc, 0);
+    const errors = results.filter(result => result.error !== null).map(result => result.error);
     res.render("import", { insertedRows, updatedRows, errors});
 })
 
